5b1024c1205c7ffea6d46848d5083c93cfd0586f
Matej Kuráň <matej.kuran@devsk.io>
2024-05-05 18:30:34 +0200

    adding comments

--- markiza-assignment-2.py
+++ markiza-assignment-2.py
@@ -2,10 +2,10 @@ import pandas as pd
 import glob
 
 if __name__ == '__main__':
-    filenames = glob.glob('csv_files/*.csv')
-    for filename in filenames:
+    filenames = glob.glob('csv_files/*.csv')  # get all filenames
+    for filename in filenames:  # iterate over filenames
         print(filename)
-        dataFrame = pd.read_csv(filename, sep=',')
-        males = dataFrame[dataFrame['gender'] == 'Male']
-        sum_of_male_age = males['vek'].sum()
+        dataFrame = pd.read_csv(filename, sep=',')  # reading csv
+        males = dataFrame[dataFrame['gender'] == 'Male']  # filtering just males
+        sum_of_male_age = males['vek'].sum()  # counting sum of ages
         print('Sum of age for this file is:', int(sum_of_male_age))
